054b969eb440bcbb17a6a3e0fc3812019e039545
the_nza <the_nza@protonmail.com>
2020-11-02 20:37:06 +0300

    Add smooth scroll to homepage and new hero img

--- src/js/parts/homepage.js
+++ src/js/parts/homepage.js
@@ -1,5 +1,21 @@
+import smoothscroll from 'smoothscroll-polyfill';
+
 export default function Homepage() {
 	if (document.querySelector(".home__main")) {
-
+		homeScrollFunc();
 	}
+}
+
+function homeScrollFunc() {
+	smoothscroll.polyfill();
+
+	const btn = document.querySelector(".home_hero__icon");
+
+	btn.addEventListener("click", function () {
+		window.scroll({
+			top: document.querySelector(".home_hero").clientHeight - 20,
+			left: 0,
+			behavior: "smooth"
+		});
+	})
 }
