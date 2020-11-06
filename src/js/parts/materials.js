c648b7c779d289763a3b0b42d10f26562bd8dd35
the_nza <the_nza@protonmail.com>
2020-11-06 17:25:09 +0300

    Add Styles for modal window and basic scripts(show/hide and some safari fix)

--- src/js/parts/materials.js
+++ src/js/parts/materials.js
@@ -1,3 +1,5 @@
+import { doc } from "prettier";
+
 function toggleCards() {
 	const categories = document.querySelectorAll(".mcat");
 	categories.forEach(cat => {
@@ -16,11 +18,45 @@ function toggleCards() {
 
 }
 
+function modal() {
+	const allBtns = document.querySelectorAll(".mcat__showall");
+	const modal = document.querySelector(".materials__all");
+	const closeBtn = modal.querySelector(".matfull__close");
+
+	allBtns.forEach(btn => {
+		btn.addEventListener("click", e => {
+			/* For track what API call must use */
+			console.log(e.target.dataset.slug);
+
+			modal.classList.add("materials__all--active", "matfull__show");
+			document.body.style.overflow = "hidden";
+		});
+	});
+
+	closeBtn.addEventListener("click", () => {
+		modal.classList.remove("materials__all--active", "matfull__show");
+		document.body.style.overflow = "auto";
+	});
+}
+
+/* 
+* detectSafari just add specific class 
+* for disable hack in safari browsers
+*/
+function detectSafari() {
+	const usrAgnt = String(window.navigator.userAgent);
+	if (usrAgnt.includes("Safari") && !usrAgnt.includes("Chrome")) {
+		document.querySelector(".matfull__cards").classList.add("matfull__cards--safari")
+	}
+}
+
 
 
 
 export default function Materials() {
 	if (document.querySelector(".materials")) {
 		toggleCards();
+		detectSafari();
+		modal();
 	}
 }
